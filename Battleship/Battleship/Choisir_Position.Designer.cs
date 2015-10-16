92737cc1cc2ad5d6be5d0ff93c0fbeb9326359e1
DominicMC <201356743@P104-05.clg.priv>
2015-10-16 12:07:53 -0400

    dfgdfg

--- Battleship/Battleship/Choisir_Position.Designer.cs
+++ Battleship/Battleship/Choisir_Position.Designer.cs
@@ -149,6 +149,9 @@
             this.label19 = new System.Windows.Forms.Label();
             this.label20 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BTN_J10
@@ -1494,6 +1497,8 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.ForeColor = System.Drawing.Color.Lime;
             this.groupBox1.Location = new System.Drawing.Point(602, 35);
@@ -1502,6 +1507,26 @@
             this.groupBox1.TabIndex = 220;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(28, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(28, 440);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Choisir_Position
             // 
@@ -1633,6 +1658,7 @@
             this.Name = "Choisir_Position";
             this.Text = "Choisir_Position";
             this.Load += new System.EventHandler(this.Choisir_Position_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -1761,5 +1787,7 @@
         private System.Windows.Forms.Label label19;
         private System.Windows.Forms.Label label20;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
