43c6d0631acf8fe21b5f77e801d37a71ae906efe
MelBeee <mel.beee@deepteal.com>
2015-10-16 08:30:44 -0400

    Qques petites modif

--- Battleship/Battleship/Menu.Designer.cs
+++ Battleship/Battleship/Menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Battleship
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.label1 = new System.Windows.Forms.Label();
+            this.flashButton1 = new FlashButton.FlashButton();
+            this.flashButton2 = new FlashButton.FlashButton();
             this.SuspendLayout();
             // 
             // label1
@@ -42,16 +44,44 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Battleship";
             // 
-            // Form1
+            // flashButton1
+            // 
+            this.flashButton1.BackgroundImage = global::Battleship.Properties.Resources.Demarrer_Normal;
+            this.flashButton1.ImageClick = global::Battleship.Properties.Resources.Demarrer_Click;
+            this.flashButton1.ImageDisable = global::Battleship.Properties.Resources.Demarrer_Disable;
+            this.flashButton1.ImageNeutral = global::Battleship.Properties.Resources.Demarrer_Normal;
+            this.flashButton1.ImageOver = global::Battleship.Properties.Resources.Demarrer_Over;
+            this.flashButton1.Location = new System.Drawing.Point(77, 138);
+            this.flashButton1.Name = "flashButton1";
+            this.flashButton1.Size = new System.Drawing.Size(286, 56);
+            this.flashButton1.TabIndex = 1;
+            // 
+            // flashButton2
+            // 
+            this.flashButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flashButton2.ImageClick = null;
+            this.flashButton2.ImageDisable = null;
+            this.flashButton2.ImageNeutral = null;
+            this.flashButton2.ImageOver = null;
+            this.flashButton2.Location = new System.Drawing.Point(696, 7);
+            this.flashButton2.Name = "flashButton2";
+            this.flashButton2.Size = new System.Drawing.Size(23, 24);
+            this.flashButton2.TabIndex = 2;
+            this.flashButton2.Click += new System.EventHandler(this.flashButton2_Click);
+            // 
+            // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(726, 463);
+            this.Controls.Add(this.flashButton2);
+            this.Controls.Add(this.flashButton1);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "Form1";
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -62,6 +92,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private FlashButton.FlashButton flashButton1;
+        private FlashButton.FlashButton flashButton2;
     }
 }
 
