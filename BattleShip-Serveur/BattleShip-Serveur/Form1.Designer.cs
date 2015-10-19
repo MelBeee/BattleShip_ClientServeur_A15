ef767adb6a4ceeac30f6f69ae07499866c3f7178
CharlieLaplante <alexylaplante@hotmail.com>
2015-10-19 13:13:40 -0400

    Serveur début

---
+++ BattleShip-Serveur/BattleShip-Serveur/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BattleShip_Serveur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LB_StatusServer = new System.Windows.Forms.Label();
+            this.Btn_DémarrerServeur = new System.Windows.Forms.Button();
+            this.Lb_JoueurConnecter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LB_StatusServer
+            // 
+            this.LB_StatusServer.AutoSize = true;
+            this.LB_StatusServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.LB_StatusServer.ForeColor = System.Drawing.Color.Red;
+            this.LB_StatusServer.Location = new System.Drawing.Point(57, 32);
+            this.LB_StatusServer.Name = "LB_StatusServer";
+            this.LB_StatusServer.Size = new System.Drawing.Size(148, 18);
+            this.LB_StatusServer.TabIndex = 0;
+            this.LB_StatusServer.Text = "SERVEUR FERMER";
+            // 
+            // Btn_DémarrerServeur
+            // 
+            this.Btn_DémarrerServeur.Location = new System.Drawing.Point(88, 135);
+            this.Btn_DémarrerServeur.Name = "Btn_DémarrerServeur";
+            this.Btn_DémarrerServeur.Size = new System.Drawing.Size(75, 23);
+            this.Btn_DémarrerServeur.TabIndex = 1;
+            this.Btn_DémarrerServeur.Text = "Démarrer Serveur";
+            this.Btn_DémarrerServeur.UseVisualStyleBackColor = true;
+            this.Btn_DémarrerServeur.Click += new System.EventHandler(this.Btn_DémarrerServeur_Click);
+            // 
+            // Lb_JoueurConnecter
+            // 
+            this.Lb_JoueurConnecter.AutoSize = true;
+            this.Lb_JoueurConnecter.Location = new System.Drawing.Point(88, 85);
+            this.Lb_JoueurConnecter.Name = "Lb_JoueurConnecter";
+            this.Lb_JoueurConnecter.Size = new System.Drawing.Size(0, 13);
+            this.Lb_JoueurConnecter.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 170);
+            this.Controls.Add(this.Lb_JoueurConnecter);
+            this.Controls.Add(this.Btn_DémarrerServeur);
+            this.Controls.Add(this.LB_StatusServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LB_StatusServer;
+        private System.Windows.Forms.Button Btn_DémarrerServeur;
+        private System.Windows.Forms.Label Lb_JoueurConnecter;
+    }
+}
+
---
+++ BattleShip-Serveur/BattleShip-Serveur/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace BattleShip_Serveur
+{
+    public partial class Form1 : Form
+    {
+        TcpListener socketServeur;
+        Boolean ServeurOuvert = false;
+        IPAddress adresseIp;
+        TcpClient[] TabClient;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_DémarrerServeur_Click(object sender, EventArgs e)
+        {
+            if(!ServeurOuvert)
+            {         
+                OuvrirServeur();
+                ServeurOuvert = true;
+            }
+            else
+            {
+                FermerServeur();
+            }            
+        }
+
+        
+        private void OuvrirServeur()
+        {
+                       
+            try
+            {
+                adresseIp = IPAddress.Parse("127.0.0.1");
+                socketServeur = new TcpListener(adresseIp, 1234);
+                socketServeur.Start();
+                Lb_JoueurConnecter.Text = "En attente de joueur...";
+                LB_StatusServer.Text = "SERVEUR OUVERT";
+                LB_StatusServer.ForeColor = Color.Green;
+                Btn_DémarrerServeur.Text = "Déconnecter";
+                Btn_DémarrerServeur.Enabled = false;
+                            
+
+                for (int nbClient = 0; nbClient < 1; nbClient++)
+                {
+                    this.Refresh();
+                    TabClient[nbClient] = socketServeur.AcceptTcpClient();
+                    if (nbClient == 0)
+                        Lb_JoueurConnecter.Text = "Joueur trouvé en attente d'un autre joueur...";
+                    else if(nbClient == 1)
+                        Lb_JoueurConnecter.Text = "Deuxième Joueur Trouvé démarage de la partie...";
+
+                }                
+
+            }
+            catch(SocketException ex)
+            {
+
+            }                 
+        }
+        private void FermerServeur()
+        {
+            socketServeur.Stop();
+            socketServeur = null;
+        }
+    }
+}
